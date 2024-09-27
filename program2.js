d7b60fda0ccccec9ccf9f9f6cb525aa326b5c43c
ShivaniGupta <vanigupta2428@gmail.com>
2024-09-27 14:46:48 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,10 +1,28 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var romanToInt = function(s) {
+    const roman_to_int = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    let prev_value = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        let current_value = roman_to_int[s[i]];
+        if (current_value > prev_value) {
+            total += current_value - 2 * prev_value;
+        } else {
+            total += current_value;
+        }
+        prev_value = current_value;
+    }
+    
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
